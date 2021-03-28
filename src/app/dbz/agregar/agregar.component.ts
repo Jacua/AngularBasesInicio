e8e133fa36cbcd5dddbfc2635beb001c6a53d334
Jonathan Acuña <jacuabarrios@gmail.com>
2021-03-27 20:04:27 -0600

    segundo commit servicios y modularizacion de logica

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html'
+})
+
+export class AgregarComponent  {
+
+  @Input() nuevo: Personaje = {
+    nombre: '',
+    poder:0
+  }
+
+  //Output Sirve para emitir eventos y poder pasarlo al componente padre
+  // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  constructor(public dbzService: DbzService){
+
+  }
+
+  agregar(){
+
+    if(this.nuevo.nombre.trim().length === 0){
+
+      return;
+    }
+
+    this.dbzService.agregarPersonaje(this.nuevo);
+
+    // this.onNuevoPersonaje.emit(this.nuevo);
+
+    this.nuevo = {nombre: '', poder: 0}
+  }
+
+}
